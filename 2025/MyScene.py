9cd3b754844a94fbea361771a5373143e6a90818
Tharun <bruceleetarun282@gmail.com>
2025-09-22 17:15:01 +0530

    update_6

--- 2025/MyScene.py
+++ 2025/MyScene.py
@@ -3,9 +3,11 @@ from manimlib import *
 class SquareToCircle(InteractiveScene):
     def construct(self):
         square = Square(side_length=2)
-        circle = Circle(radius=3)
+        circle = Circle(radius=5)
         self.add(square, circle)
-        self.play(Transform(square, circle))
-        self.add(Axes())
-        self.wait(1)
+        self.play(Transform(square, circle), run_time=8)
+        self.wait(2)
         
+# class mob(Mobject):
+#     def construct(self):
+         
